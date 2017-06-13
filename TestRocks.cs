9cacbf87318802dca102920d60c60b72fa5608e1
ravengerUA <yurisikorsky@gmail.com>
2017-06-13 22:05:21 +0300

    init

---
+++ TestRocks.cs
@@ -0,0 +1,23 @@
+﻿using RocksDbSharp;
+using Xunit;
+
+namespace TestExample
+{
+    public class TestRocks
+    {
+        [Fact]
+        public void Test()
+        {
+            var options = new DbOptions().SetCreateIfMissing(true).EnableStatistics();
+            using (var db = RocksDb.Open(options, "rocksdb_simple_hl_example"))
+            {
+                string value = db.Get("key");
+                db.Put("key", "value");
+                value = db.Get("key");
+                string iWillBeNull = db.Get("non-existent-key");
+                Assert.Equal("null", iWillBeNull);
+                db.Remove("key");
+            }
+        }
+    }
+}
